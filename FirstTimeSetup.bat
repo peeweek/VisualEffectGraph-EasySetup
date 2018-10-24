049ccbbb79ab589b05166a1401cdf81f9697f4b2
Thomas ICHÉ <peeweek@gmail.com>
2018-10-24 09:20:22 -0700

    Fixed mkdir folder name

--- FirstTimeSetup.bat
+++ FirstTimeSetup.bat
@@ -1,6 +1,6 @@
 @echo off
 ECHO Installing SRP/HDRP/VisualEffectGraph...
-md SRP
+md GitHub
 git clone -b release/2018.3 https://github.com/Unity-Technologies/ScriptableRenderPipeline.git ./GitHub
 
 pause
